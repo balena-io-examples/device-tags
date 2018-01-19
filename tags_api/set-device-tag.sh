792d321c912a2e6036502cc9a503d4cc994fd043
Thodoris Greasidis <thgreasi@gmail.com>
2018-01-19 08:39:56 +0200

    Add curl tag scripts

---
+++ tags_api/set-device-tag.sh
@@ -0,0 +1,26 @@
+API_KEY=$RESIN_API_KEY
+if [ "$USER_API_KEY" ] ; then
+	echo "using USER_API_KEY"
+	API_KEY=$USER_API_KEY
+fi
+
+TAG_KEY=$1
+TAG_VALUE=$2
+
+RESIN_DEVICE_ID=$(curl "https://api.resin.io/v3/device?\$select=id,uuid&\$filter=uuid%20eq%20'$RESIN_DEVICE_UUID'" -H "Authorization: Bearer $API_KEY" | jq '.d[0].id')
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $SCRIPTPATH
+TAG_EXISTS=$(sh get-device-tag.sh "$TAG_KEY" "$RESIN_DEVICE_ID" | jq ".d[0] != null")
+
+if [ "$TAG_EXISTS" = "false" ] ; then
+	curl -X POST "https://api.resin.io/v3/device_tag" \
+		-H "Authorization: Bearer $API_KEY" \
+		-H "Content-Type: application/json" \
+		--data-binary '{"tag_key":"'$TAG_KEY'","device":'$RESIN_DEVICE_ID',"value":"'$TAG_VALUE'"}'
+else
+	curl -X PATCH "https://api.resin.io/v3/device_tag?\$filter=tag_key%20eq%20'$TAG_KEY'" \
+		-H "Authorization: Bearer $API_KEY" \
+		-H "Content-Type: application/json" \
+		--data-binary '{"tag_key":"'$TAG_KEY'", "device":'$RESIN_DEVICE_ID', "value":"'$TAG_VALUE'"}'
+fi
