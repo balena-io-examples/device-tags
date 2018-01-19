2d3e0a25737ca5e950897f90e9bcdbf00ccad09d
Thodoris Greasidis <thgreasi@gmail.com>
2018-01-19 08:52:54 +0200

    Add SDK tag scripts

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var app = express();
+var ResinSdk = require('resin-sdk');
+var sdk;
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
+  if (sdk) {
+    // set the 'stats.lastrequest' tag but w/o blocking the response
+    sdk.models.device.tags
+      .set(process.env.RESIN_DEVICE_UUID, 'stats.lastrequest', Date.now())
+      .catch(function(e) {
+        console.error('Error while setting stats.lastrequest tag', e);
+      });
+  }
   res.send('Hello World!');
 });
 
 //start a server on port 80 and log its start to our console
 var server = app.listen(80, function () {
+  if (process.env.USER_API_KEY) {
+    sdk = ResinSdk();
+    sdk.auth.loginWithToken(process.env.USER_API_KEY);
+    console.log('Initialized sdk with $USER_API_KEY env var');
+  } else {
+    sdk = ResinSdk({ apiKey: process.env.RESIN_API_KEY });
+    console.log('Initialized sdk with $RESIN_API_KEY container env var');
+  }
 
   var port = server.address().port;
   console.log('Example app listening on port ', port);
