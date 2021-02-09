c8bd61376ab218e0cc134d3e565199d8cc322b55
Thodoris Greasidis <thgreasi@gmail.com>
2021-02-09 19:19:19 +0200

    Rename the curl examples folder to be clear what it is
    Change-type: patch

--- main/start.sh
+++ main/start.sh
@@ -1,4 +1,4 @@
 echo "setting 'stats.laststart' tag"
-sh tags_api/set-device-tag.sh "stats.laststart" $(date +%s)
+sh curl_tags_api/set-device-tag.sh "stats.laststart" $(date +%s)
 echo "starting application"
 npm start
