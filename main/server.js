78baa4313808bfe0a6c63a5b91c8e5632f7c4038
Thodoris Greasidis <thgreasi@gmail.com>
2021-02-09 19:20:03 +0200

    Update balena-sdk to v15
    Change-type: major

--- main/server.js
+++ main/server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
-var SdkInstanceFactory = require('balena-sdk');
-var sdk;
+const express = require('express');
+const app = express();
+const { getSdk } = require('balena-sdk');
+let sdk;
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
@@ -13,19 +13,21 @@ app.get('/', function (req, res) {
         console.error('Error while setting stats.lastrequest tag', e);
       });
   }
-  res.send('Hello World!');
+  res.send('Hello World from device tags example!');
 });
 
 //start a server on port 80 and log its start to our console
-var server = app.listen(80, function () {
-
-  sdk = SdkInstanceFactory();
+const server = app.listen(80, function () {
+  sdk = getSdk({
+    // only required if the device is not running on balena-cloud.com
+    apiUrl: process.env.BALENA_API_URL
+  });
   sdk.auth.logout();
   sdk.auth.loginWithToken(process.env.BALENA_API_KEY);
 
   sdk.models.device.tags.set(process.env.BALENA_DEVICE_UUID, 'stats.lastserverstart', Date.now());
 
-  var port = server.address().port;
+  const port = server.address().port;
   console.log('Example app listening on port ', port);
 
 });
