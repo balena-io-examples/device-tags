4f71f39868bf634fa53b39ef6275ac7375ae6889
Thodoris Greasidis <thgreasi@gmail.com>
2021-02-09 19:48:32 +0200

    server.js: Refactor the setup to use async await
    Change-type: patch

--- main/server.js
+++ main/server.js
@@ -8,26 +8,29 @@ app.get('/', function (req, res) {
   if (sdk) {
     // set the 'stats.lastrequest' tag but w/o blocking the response
     sdk.models.device.tags
-      .set(process.env.BALENA_DEVICE_UUID, 'stats.lastrequest', Date.now())
+      .set(process.env.BALENA_DEVICE_UUID, 'stats.last_request', Date.now())
       .catch(function(e) {
-        console.error('Error while setting stats.lastrequest tag', e);
+        console.error('Error while setting stats.last_request tag', e);
       });
   }
   res.send('Hello World from device tags example!');
 });
 
 //start a server on port 80 and log its start to our console
-const server = app.listen(80, function () {
+const server = app.listen(80, async function () {
   sdk = getSdk({
     // only required if the device is not running on balena-cloud.com
     apiUrl: process.env.BALENA_API_URL
   });
-  sdk.auth.logout();
-  sdk.auth.loginWithToken(process.env.BALENA_API_KEY);
 
-  sdk.models.device.tags.set(process.env.BALENA_DEVICE_UUID, 'stats.lastserverstart', Date.now());
+  try {
+    await sdk.auth.logout();
+    await sdk.auth.loginWithToken(process.env.BALENA_API_KEY);
+    await sdk.models.device.tags.set(process.env.BALENA_DEVICE_UUID, 'stats.lastserverstart', Date.now());
+  } catch (err) {
+    console.error('Error while setting stats.lastserverstart tag', err);
+  }
 
   const port = server.address().port;
   console.log('Example app listening on port ', port);
-
 });
