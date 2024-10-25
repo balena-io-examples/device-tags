fda6ad91cfb2d4a223f9f2a79d8960e1d938fd60
Thodoris Greasidis <thgreasi@gmail.com>
2024-10-25 16:17:32 +0300

    Set an ISO date string as the tag values
    Change-type: minor

--- device_tags_node/start.sh
+++ device_tags_node/start.sh
@@ -1,4 +1,4 @@
 echo "setting 'stats.laststart' tag"
-sh curl_tags_api/set-device-tag.sh "stats.last_application_start" $(date +%s)
+sh curl_tags_api/set-device-tag.sh "stats.last_application_start" $(date +"%Y-%m-%dT%H:%M:%S%z")
 echo "starting application"
 npm start
